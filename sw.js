3e07fe8ae6cef1e8980376fa12ef801c97b077f6
Allen Dean <allen.dean@vokal.io>
2015-12-09 10:44:09 -0600

    Adding index file to /gallery.

--- sw.js
+++ sw.js
@@ -1,33 +1,33 @@
-this.addEventListener('install', function(event) {
+this.addEventListener( "install", function ( event ) {
   event.waitUntil(
-    caches.open('v1').then(function(cache) {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/style.css',
-        '/app.js',
-        '/image-list.js',
-        '/star-wars-logo.jpg',
-        '/gallery/',
-        '/gallery/bountyHunters.jpg',
-        '/gallery/myLittleVader.jpg',
-        '/gallery/snowTroopers.jpg'
-      ]);
-    })
+    caches.open( "v1" ).then( function ( cache ) {
+      return cache.addAll( [
+        "/",
+        "/index.html",
+        "/style.css",
+        "/app.js",
+        "/image-list.js",
+        "/star-wars-logo.jpg",
+        "/gallery/",
+        "/gallery/bountyHunters.jpg",
+        "/gallery/myLittleVader.jpg",
+        "/gallery/snowTroopers.jpg"
+      ] );
+    } )
   );
-});
+} );
 
-this.addEventListener('fetch', function(event) {
+this.addEventListener( "fetch", function ( event ) {
   var response;
-  event.respondWith(caches.match(event.request).catch(function() {
-    return fetch(event.request);
-  }).then(function(r) {
+  event.respondWith( caches.match( event.request ).catch( function () {
+    return fetch( event.request );
+  } ).then( function ( r ) {
     response = r;
-    caches.open('v1').then(function(cache) {
-      cache.put(event.request, response);
-    });
+    caches.open( "v1" ).then( function ( cache ) {
+      cache.put( event.request, response );
+    } );
     return response.clone();
-  }).catch(function() {
-    return caches.match('/gallery/myLittleVader.jpg');
-  }));
-});
+  } ).catch( function () {
+    return caches.match( "/gallery/myLittleVader.jpg" );
+  } ) );
+} );
